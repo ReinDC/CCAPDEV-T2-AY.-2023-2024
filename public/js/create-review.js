768185585d992b633e332e2fca5a6449e2b729b7
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-07-23 16:53:47 +0800

    Added script now for form submission.

--- public/js/create-review.js
+++ public/js/create-review.js
@@ -1,3 +1,48 @@
-function goBack() {
-    window.history.back();
-}
+const form = document.getElementById('reviewSubmission');
+const postBtn = document.getElementById('postBtn');
+const recoBtn = document.getElementById('recoBtn');
+const notRecoBtn = document.getElementById('notRecoBtn');
+
+
+recoBtn.addEventListener('click', () => {
+    document.getElementById('isRecommended').value = 'true';
+})
+
+notRecoBtn.addEventListener('click', () => {
+    document.getElementById('isRecommended').value = 'false';
+})
+
+
+postBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+    
+
+    const myObj = { 
+        reviewID: formData.get('reviewID'),
+        reviewerID: formData.get('reviewerID'),
+        resturantID: formData.get('resturantID'),
+        reviewContent: formData.get('reviewContent'),
+        reviewTitle: formData.get('reviewTitle'),
+        isRecommended: formData.get('isRecommended') === "true",
+        helpfulCount: parseInt(formData.get('helpfulCount'), 10),
+        notHelpfulCount: parseInt(formData.get('notHelpfulCount'), 10)
+    };
+
+    const jString = JSON.stringify(myObj);
+
+    try{
+        const response = await fetch("/submit-review",{
+            method: 'POST',
+            body: jString,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }catch(error){
+        console.error(error);
+        showCustomAlert("An unexpected error occurred.")
+    }
+    
+});
